4a674fec0d6710cb863a575560522eb826ff660b
thayer6 <casey.thayer6@gmail.com>
2021-01-27 13:57:41 -0800

    first commit

---
+++ supertest/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from supertest.users.models import User
+from supertest.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
